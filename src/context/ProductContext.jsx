2c9365513ff3e12954182480310ff872c5ed4ebb
anthony sanchez <anthonysa0813@gmail.com>
2022-04-08 21:19:15 -0500

    little changes

--- src/context/ProductContext.jsx
+++ src/context/ProductContext.jsx
@@ -3,7 +3,13 @@ import React, { createContext, useState } from "react";
 export const ProductoContext = createContext();
 
 const ProductProvider = ({ children }) => {
-  const [productArr, setproductArr] = useState([]);
+  const [productArr, setproductArr] = useState({
+    products: [],
+    totalPrice: 0,
+  });
+
+  const totalPriceFunc = (arr) => {};
+
   return (
     <ProductoContext.Provider value={{ productArr, setproductArr }}>
       {children}
